e493bc8d73dc20305aca5d38ff526c346b4b97e2
Taron Abgaryan <taronabg@mail.ru>
2019-03-09 00:36:16 +0400

    kendo ui added in webpack

---
+++ models.js
@@ -0,0 +1,10 @@
+export  var  Car = [
+    { model: "Bmw", price: "5000$", country: "Armenia", picture: "img/bmw.png", id: 1 },
+    { model: "Mercedes-Benz", price: "8000$", country: "Georgia", picture: "img/m.png", id: 1 },
+    { model: "Audi", price: "3500$", country: "Albania", picture: "img/audi.png", id: 3 },
+    { model: "Opel", price: "1500$", country: "Francia", picture: "img/opel.png", id: 4 },
+    { model: "Ferrari", price: "15000$", country: "Francia", picture: "img/ferari.png", id: 5 },
+    { model: "Bmw M6", price: "7500$", country: "USA", picture: "img/m6.png", id: 6 },
+    { model: "Tesla", price: "150500$", country: "Francia", picture: "img/tesla.png", id: 7 },
+    { model: "mercedes_Benz-Cl", price: "15500$", country: "Germania", picture: "img/cl.png", id: 8 }
+  ];
