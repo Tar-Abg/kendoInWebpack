5261e96527fd4fc544001d927212ddd2508747cf
Taron Abgaryan <taronabg@mail.ru>
2019-04-07 21:44:02 +0400

    added new kendo menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,6 +1,6 @@
 var $ = require('jquery')
 import { dataMenu } from  "../models";
-import { secndMenu } from "./models"
+import { secndMenu } from "../models"
 import { createNavabr } from  "kendonavbar";
 import { createSecndNavabr} from "kendonavbar"
 
--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ require('./css/style.css');
 
 require ('./components/menu.js');
 // require ('./components/kendoGrid');
-// require('../node_modules/kendo-ui-core/css/web/kendo.default.min.css') 
-// require('../node_modules/@progress/kendo-ui/css/web/kendo.common.min.css')
+require('../node_modules/kendo-ui-core/css/web/kendo.default.min.css') 
+require('../node_modules/@progress/kendo-ui/css/web/kendo.common.min.css')
