77ff7d3324b07f38f589e6f763b196ecb20fcad2
Taron Abgaryan <taronabg@mail.ru>
2019-04-07 21:04:57 +0400

    chenged menu moduls

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,11 @@
 var $ = require('jquery')
-import { dataMenu } from  "../models"
+import { dataMenu } from  "../models";
+import { secndMenu } from "./models"
 import { createNavabr } from  "kendonavbar";
+import { createSecndNavabr} from "kendonavbar"
 
 createNavabr( $ ("body"), dataMenu);
+createSecndNavabr($("body"), secndMenu);
 
 
 
