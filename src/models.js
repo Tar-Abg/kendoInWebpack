fcca5285b1d0046b341989144c5effbf370f2d97
Taron Abgaryan <taronabg@mail.ru>
2019-04-07 21:00:01 +0400

    changed data from models

--- src/models.js
+++ src/models.js
@@ -9,44 +9,60 @@ export  var  cars = [
     { model: "mercedes_Benz-Cl", price: "15500$", country: "Germania", picture: "img/cl.png", id: 8 }
   ];
 
-export var  dataMenu = [
-  {
-      text: "Model",
-      items: [
-      {
-          text: "Bmw",
-          items: [
-          { text: "BMW X1" },
-          { text: "BMW 3 Series GT" },
-          { text: "BMW i8" },
-          { text: "BMW X4" },
-          { text: "BMW X6" },
-          ]
-      },
-      {
-          text: "Mercedes-Benz",
-          items: [
-          { text: "GLA 45 AMG" },
-          { text: "AMG GT" },
-          { text: "GLE" },
-          { text: "GLE 2019" },
-          ],
-      },
-      ],
-  },
-  {
-      text: "German Cars",
-      items: [
-      { text: "Audi" },
-      { text: "Opel" },
-      { text: "Volkswagen" },
-      { text: "Ford-Werke GmbH" }
-      ],
-  },
-  {
-      text: "Contact",
-  },
-  {
-      text: "About",
-  }
-  ]
+  export var dataMenu = [
+    {
+        text: "", imageUrl: "https://img.icons8.com/ios-glyphs/30/000000/home.png",
+        url: "#grid",
+        cssClass:"menuItem"
+    },
+    {
+        text: "<span class=\"k-icon k-i-track-changes-enable \"></span>",encoded: false,
+        url: "#grid",
+        cssClass:"menuItem"
+    },
+    {
+        text: "<span class=\"k-icon k-i-success \"></span>",encoded: false,
+        url: "#grid",
+        cssClass:"menuItem"
+    },
+    {
+        text: "<span class=\"k-icon k-i-zoom \"></span>",encoded: false,
+        url: "#grid",
+        cssClass:"menuItem"
+    },
+    {
+        text: "<span class=\"k-icon k-i-notification k-i-bell\"></span>",encoded: false,
+        url: "#",
+        cssClass:"menuItem"
+    },
+    {
+        text: "<span class=\"k-icon k-i-gear k-icon-64\"></span>",encoded: false,
+        url: "#",
+        cssClass:"menuItem"
+    },
+    {
+        text: "", imageUrl:"https://img.icons8.com/ios-glyphs/30/000000/question-mark.png",
+        url: "#",
+        cssClass:"last-item"
+    }
+  
+];
+export var secndMenu =  [
+    {
+        text: "", imageUrl: "https://img.icons8.com/ios-glyphs/60/000000/restart.png",
+        url: "#grid",
+        cssClass:"sencndMenuItem"
+    },
+    {
+        text: "", imageUrl: "src/assets/avatar-5.jpg",
+        url: "#grid",
+        cssClass:"sencndMenuItem-lastItem img"
+    },
+    {
+        text: "", imageUrl: "https://img.icons8.com/ios-glyphs/30/000000/chain-end.png",
+        url: "#grid",
+        cssClass:"sencndMenuItem-lastItem"
+        
+    }
+]
+
