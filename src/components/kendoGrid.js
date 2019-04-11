0c385651383c1c3c71c27f67a6cb27b5bcc18efd
Taron Abgaryan <taronabg@mail.ru>
2019-04-11 17:28:02 +0400

    added new grid

--- src/components/kendoGrid.js
+++ src/components/kendoGrid.js
@@ -1,7 +1,8 @@
 var $ = require("jquery");
 import { createGrid } from "kendoGridModule";
 import { cars } from "../models";
-import { credeKendoGrid } from "../../node_modules/kendoGrid/src/main";
+import { gridColumn } from "../models"
+// import { credeKendoGrid } from "../../node_modules/kendoGrid/src/main";
 
-createGrid($("body"), cars);
-credeKendoGrid($("#kendoGrid"));
+createGrid($("body"), cars,gridColumn, "Rapprochments", "11/04/2019");
+// credeKendoGrid($("#kendoGrid"));
--- src/models.js
+++ src/models.js
@@ -1,12 +1,12 @@
 export  var  cars = [
-    { model: "Bmw", price: "5000$", country: "Armenia", picture: "img/bmw.png", id: 1 },
-    { model: "Mercedes-Benz", price: "8000$", country: "Georgia", picture: "img/m.png", id: 2 },
-    { model: "Audi", price: "3500$", country: "Albania", picture: "img/audi.png", id: 3 },
-    { model: "Opel", price: "1500$", country: "Francia", picture: "img/opel.png", id: 4 },
-    { model: "Ferrari", price: "15000$", country: "Francia", picture: "img/ferari.png", id: 5 },
-    { model: "Bmw M6", price: "7500$", country: "USA", picture: "img/m6.png", id: 6 },
-    { model: "Tesla", price: "150500$", country: "Francia", picture: "img/tesla.png", id: 7 },
-    { model: "mercedes_Benz-Cl", price: "15500$", country: "Germania", picture: "img/cl.png", id: 8 }
+    { model: "FINEO_101_SOGEEU", price: "", country: "", picture: "", id: "" },
+    { model: "FINEO_102_SOGEEU", price: "", country: "", picture: "", id: "" },
+    { model: "FINEO_100_SOGEEU", price: "", country: "", picture: "", id: "" },
+    { model: "FINEO_100_SOGEEU", price: "", country: "", picture: "", id: "" },
+    { model: "FINEO_200_SOGEEU", price: "", country: "", picture: "", id: "" },
+    { model: "FINEO_200_SOGEEU", price: "", country: "", picture: "", id: "" },
+    { model: "FINEO_200_SOGEEU", price: "", country: "", picture: "", id: "" },
+    { model: "FINEO_101_SOGEEU", price: "", country: "", picture: "", id: "" }
   ];
 
   export var dataMenu = [
@@ -65,4 +65,16 @@ export var secndMenu =  [
         
     }
 ]
+export var gridColumn = [
+            { title: " ", field: "model",
+              attributes: {
+                "class": "first-cell",
+                style: "width:120px "
+              } 
+            },
+            { title: "None Rappr", field: "id" },
+            { title: "Rapproches", field: "price" },
+            { title: "None Rappr", field: "country" },
+            { title: "Total", field: "country" }
+        ]
 
