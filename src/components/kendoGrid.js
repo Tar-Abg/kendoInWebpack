2e9ba7ba9135775bdf180064fa7a775fe08ff81e
Taron Abgaryan <taronabg@mail.ru>
2019-04-11 19:33:22 +0400

    added secnd grid

--- src/components/kendoGrid.js
+++ src/components/kendoGrid.js
@@ -1,8 +1,11 @@
 var $ = require("jquery");
 import { createGrid } from "kendoGridModule";
 import { cars } from "../models";
+import { dataForSecndGrid } from "../models"
 import { gridColumn } from "../models"
+import { gridColumnToo } from "../models"
 // import { credeKendoGrid } from "../../node_modules/kendoGrid/src/main";
 
 createGrid($("body"), cars,gridColumn, "Rapprochments", "11/04/2019");
+createGrid($("body"), dataForSecndGrid,gridColumnToo, "Situation des comptes", "")
 // credeKendoGrid($("#kendoGrid"));
--- src/models.js
+++ src/models.js
@@ -5,8 +5,18 @@ export  var  cars = [
     { model: "FINEO_100_SOGEEU", price: "", country: "", picture: "", id: "" },
     { model: "FINEO_200_SOGEEU", price: "", country: "", picture: "", id: "" },
     { model: "FINEO_200_SOGEEU", price: "", country: "", picture: "", id: "" },
-    { model: "FINEO_200_SOGEEU", price: "", country: "", picture: "", id: "" },
-    { model: "FINEO_101_SOGEEU", price: "", country: "", picture: "", id: "" }
+    // { model: "FINEO_200_SOGEEU", price: "", country: "", picture: "", id: "" },
+    // { model: "FINEO_101_SOGEEU", price: "", country: "", picture: "", id: "" }
+  ];
+  export  var  dataForSecndGrid = [
+    { model: "FINEO_101_SOGEEU", price: "599 099.5", country: "599 099.5", picture: "599 099.5", id: "599 099.5" },
+    { model: "FINEO_102_SOGEEU", price: "0.00", country: "0.00", picture: "0.00", id: "0.00" },
+    { model: "FINEO_100_SOGEEU", price: "-555. 44.5", country: "-555. 44.5", picture: "-555. 44.5", id: "-555. 44.5" },
+    { model: "FINEO_100_SOGEEU", price: "0.00", country: "0.00", picture: "0.00", id: "0.00" },
+    { model: "FINEO_200_SOGEEU", price: "162 25.44", country: "162 25.44", picture: "162 25.44", id: "162 25.44" },
+    { model: "FINEO_200_SOGEEU", price: "1 000 00", country: "1 000 00", picture: "1 000 00", id: "1 000 00" },
+    { model: "FINEO_200_SOGEEU", price: "-10 110.5", country: "-10 110.5", picture: "-10 110.5", id: "-10 110.5" },
+    // { model: "FINEO_101_SOGEEU", price: "", country: "", picture: "", id: "" }
   ];
 
   export var dataMenu = [
@@ -69,12 +79,25 @@ export var gridColumn = [
             { title: " ", field: "model",
               attributes: {
                 "class": "first-cell",
-                style: "width:120px "
+                style: "width:100px;color: hsl(208, 98%, 20%) "
               } 
             },
             { title: "None Rappr", field: "id" },
             { title: "Rapproches", field: "price" },
             { title: "None Rappr", field: "country" },
             { title: "Total", field: "country" }
-        ]
+        ];
+export var gridColumnToo = [
+    { title: " ", field: "model",
+        attributes: {
+        "class": "first-cell",
+        style: "color: hsl(208, 98%, 20%); width:100px "
+        } 
+    },
+    { title: "04/04/19", field: "id" },
+    { title: "05/04/19", field: "price" },
+    { title: "06/04/19", field: "country" },
+    { title: "07/04/19", field: "country" },
+    { title: "08/04/19", field: "country" }
+]
 
